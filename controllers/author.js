7bdd7ca1f3ab2a43140ff0099de21d744b5fe306
tj-04 <tejas@evergreenclub.in>
2022-04-17 08:43:36 +0530

    nue set

--- controllers/author.js
+++ controllers/author.js
@@ -26,4 +26,17 @@ async function authorValue(req, res) {
   res.status(200).send({message: 'No one does'});
 }
 
-module.exports = {authorDetails, authorValue};
+/**
+ * Get Data
+ * @param {object} req body.
+ * @param {object} res body.
+ */
+async function authorValues(req, res) {
+  console.log('req ', req);
+  if (req.body) {
+    res.status(403).send({message: 'Who cares'});
+  }
+  res.status(200).send({message: 'No one does'});
+}
+
+module.exports = {authorDetails, authorValue, authorValues};
