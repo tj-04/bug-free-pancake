f58d29b5ca7ee3ee04de4b0ac5d25c64458672e5
tj-04 <tejas@evergreenclub.in>
2022-04-17 08:40:36 +0530

    new set

---
+++ controllers/author.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * Get Data
+ * @param {object} req body.
+ * @param {object} res body.
+ */
+async function authorDetails(req, res) {
+  console.log('req ', req);
+  if (req.body) {
+    res.status(403).send({message: 'Forbidden!!!'});
+  }
+  res.status(200).send({message: 'Done!!!'});
+}
+
+/**
+ * Get Data
+ * @param {object} req body.
+ * @param {object} res body.
+ */
+async function authorValue(req, res) {
+  console.log('req ', req);
+  if (req.body) {
+    res.status(403).send({message: 'Who cares'});
+  }
+  res.status(200).send({message: 'No one does'});
+}
+
+module.exports = {authorDetails, authorValue};
--- routes.js
+++ routes.js
@@ -1,7 +1,12 @@
 'use strict';
 
+const aD = require('./controllers/author');
+
 exports.init = (router) => {
   router.route('/').get((req, res) => {
     res.status(200).send({message: 'Online Library Dashboard'});
   });
+
+  router.route('/author').get(aD.authorDetails);
+  router.route('/author/1').get(aD.authorValue);
 };
